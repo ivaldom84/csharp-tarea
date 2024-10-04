8f2638f74a783b7f4661b4af4f1142324d2d9a40
ivaldom84 <ivaldom84@gmail.com>
2024-10-03 22:10:59 -0400

    Add files via upload

---
+++ hola_mundo/hola_mundo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hola_mundo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hola mundo");
+        }
+    }
+}
